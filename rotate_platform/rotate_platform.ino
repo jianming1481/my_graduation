4ffa67778c26ef673ee4c4a245125f4dba35a65e
Chien-Ming Lin <jianming1481@gmail.com>
2017-09-21 19:43:05 +0800

    Arduino change from client to server

--- rotate_platform/rotate_platform.ino
+++ rotate_platform/rotate_platform.ino
@@ -1,21 +1,56 @@
+/*
+ *  Control Rotate Platfom
+ */
+
 #include <DynamixelSerial.h>
 #include <ros.h>
 #include <std_msgs/String.h>
 #include <trainning_data_builder/data.h>
 
-ros::NodeHandle  nh;
-std_msgs::String str_msg;
+// Global variable
 int Pre_Position,Position; 
 char m_Position[5];
-String str;
+
+// ROS
+ros::NodeHandle  nh;
 using trainning_data_builder::data;
-ros::ServiceClient<data::Request, data::Response> client("/trainning_data_builder/save");
+
+// ROS service callback function
+void callback(const data::Request &req, data::Response &res)
+{
+  // In AX12, 5 degree equals to 14 plus
+  // If Ax12 rotate more than 5 degree, AX12 STOP
+  int tmp = 0;
+  while(tmp < 14)
+  {
+    Dynamixel.turn(1,LEFT,100);
+    // Read position from AX12
+    Position = Dynamixel.readPosition(1);
+    tmp = Position-Pre_Position;
+    
+    // Prevent degree over from 360 to 0
+    if(tmp < 0)
+    {
+      Pre_Position = Position;
+    }
+    res.success = false;
+  }
+  
+  Pre_Position = Position;
+  // AX12 Stop rotate
+  Dynamixel.turn(1,LEFT,0);
+  res.success = true;
+}
+
+// Declare the server
+ros::ServiceServer<data::Request, data::Response> server("/trainning_data_builder/save",&callback);
 
 void setup(){ // Configuration
+  // Initial ROS node
   nh.initNode();
-  nh.serviceClient(client);
-  while(!nh.connected()) nh.spinOnce();
-  nh.loginfo("Startup complete");
+  nh.advertiseService(server);
+
+  // Initial AX12
   Dynamixel.begin(1000000,2); //2=data control
   delay(1000);
   Dynamixel.setEndless(1,ON);
@@ -25,33 +60,6 @@ void setup(){ // Configuration
 }
 
 void loop(){
-  Position = Dynamixel.readPosition(1);
-  data::Request req;
-  data::Response res;
-  int tmp = Position-Pre_Position;
-  if((tmp)>14)
-  {
-     Dynamixel.turn(1,LEFT,0);
-     Pre_Position = Position;
-     req.str = "trainning_data";
-     client.call(req, res);
-     while(!res.success)
-     {
-      delay(1);
-      client.call(req, res);
-     }
-     if(res.success)
-     {
-      delay(1000);
-     }
-     delay(100);
-  }
-  if(tmp < 0)
-  {
-    Pre_Position = Position;
-  }
-  Dynamixel.turn(1,LEFT,100);
-  delay(100);
-
   nh.spinOnce();
+  delay(10);
 }
